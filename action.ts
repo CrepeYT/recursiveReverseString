1551a9b2166954d4d188fe775c0f479e5208fc5a
yodsawat <123528072+CrepeYT@users.noreply.github.com>
2023-01-25 10:44:59 +0700

    Add files via upload

---
+++ action.ts
@@ -0,0 +1,11 @@
+const text = "Hello"
+const textt = "Prayuth"
+
+function recursiveReverseString(text: string) : string{
+    if (text.length <= 0){ // Base Case
+        return ''
+    }
+    return recursiveReverseString(text.slice(1)) + text.charAt(0)
+}
+console.log(recursiveReverseString('Hello'))
+console.log(recursiveReverseString('Prayuth'))
